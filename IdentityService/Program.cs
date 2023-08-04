b0338d0c0d5dca87c9b0fea42c21f7979ff07443
Dima <dima0958757803@gmail.com>
2023-08-04 13:24:42 +0300

    added logger

--- IdentityService/Program.cs
+++ IdentityService/Program.cs
@@ -1,3 +1,6 @@
+using Core.Logging;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.AddLogging();
 
 var app = builder.Build();
 
@@ -15,6 +19,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogLogging();
 
 app.UseHttpsRedirection();
 
