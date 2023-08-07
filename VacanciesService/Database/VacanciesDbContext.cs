a616d28fdbda166e700e8d419309195fe9bb93c6
dexon03 <dima0958757803@gmail.com>
2023-08-07 22:27:01 +0300

    added models for vacancies service

--- VacanciesService/Database/VacanciesDbContext.cs
+++ VacanciesService/Database/VacanciesDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using VacanciesService.Models;
 
 namespace VacanciesService.Database;
 
@@ -8,4 +9,12 @@ public class VacanciesDbContext : DbContext
     {
         
     }
+    
+    public DbSet<Vacancy> Vacancies { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Skill> Skills { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<LocationVacancy> LocationVacancies { get; set; }
+    public DbSet<VacancySkill> VacancySkills { get; set; }
 }
---
+++ VacanciesService/Models/Location.cs
@@ -0,0 +1,8 @@
+﻿namespace VacanciesService.Models;
+
+public class Location
+{
+    public Guid Id { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+}
